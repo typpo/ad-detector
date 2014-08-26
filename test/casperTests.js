a9a2a8877cd33e7b9ff1d24aab8d3f57039806c5
Ian <typppo@gmail.com>
2014-08-26 01:14:44 -0700

    minor casper test cleanup

--- test/casperTests.js
+++ test/casperTests.js
@@ -15,8 +15,8 @@ casper.userAgent('Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.36 (KHTML, like 
 casper.options.stepTimeout = TIMEOUT;
 casper.options.onStepTimeout = function() {
   casper.echo('Test timed out');
-  this.test.assert(false);
-  //this.test.fail(this.requestUrl + ' loads in less than ' + TIMEOUT + 'ms.');
+  this.test.fail(this.requestUrl + ' loads in less than ' + TIMEOUT + 'ms.');
+  this.test.done();
   run(this.idx + 1);
 };
 
