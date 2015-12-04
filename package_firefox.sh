d5c4279c581124bff52fe97840775de8c6627cc9
Ian Webster <ianw_git@ianww.com>
2015-12-03 22:50:29 -0800

    Update for new Firefox release process.

--- package_firefox.sh
+++ package_firefox.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -e
+# This requires jpm: sudo npm install jpm -g
 
 pushd `dirname $0`
 
 ./setup_hardlinks.sh
 
 pushd firefox
-cfx xpi
-mv ad-detector.xpi ../
+jpm xpi
+mv 19b15b40-23f9-11e4-8c21-0800200c9a66@jetpack-0.20.xpi ../ad-detector.xpi
 popd
 
 popd
