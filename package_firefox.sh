6cec8f9c6db480e690573b34c2995b342c9868cf
Ian Webster <ianw_github@ianww.com>
2016-07-24 15:27:57 +0300

    make firefox release process generic

--- package_firefox.sh
+++ package_firefox.sh
@@ -7,7 +7,7 @@ pushd `dirname $0`
 
 pushd firefox
 jpm xpi
-mv 19b15b40-23f9-11e4-8c21-0800200c9a66@jetpack-0.20.xpi ../ad-detector.xpi
+mv 19b15b40-23f9-11e4-8c21-0800200c9a66@jetpack-*.xpi ../ad-detector.xpi
 popd
 
 popd
